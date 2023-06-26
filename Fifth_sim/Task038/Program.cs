f50399fbe13d6a5b2b07dfba661fb7ff554e0335
Dmitriy <skverdv@gmail.com>
2023-06-26 17:35:30 +0300

    Task038

---
+++ Fifth_sim/Task038/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив целых двухзначных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [22, 42, 15, 77, 65] => 77 - 15 = 62**
+
+Console.WriteLine("Введите размер массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = GetArray(n, 10, 99);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+Console.WriteLine($"[{string.Join(",", array)}]");
+
+int min = 99;
+int max = 0;
+foreach (int el in array)  
+{
+    if (el < min) min = el;
+    if (el > max) max = el;
+}
+Console.WriteLine($"\n Максимальный элемент: {max}\n Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {max - min}");
+
+
+
+
+

e227cc8e4ffc7f5d3940ec385f2afa1de0818dee
Dmitriy <skverdv@gmail.com>
2023-07-03 14:31:30 +0300

    Task052

---
+++ Seventh_sim/Task052/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое* элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// * Сумма всех цифр / их количество(это количество ваших строк)
+
+Console.Write("введите номер строки: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("введите номер столбца: ");
+int m = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[n, m];
+GetArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+    }
+
+Console.WriteLine();
+PrintArray(array);
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] inarray)
+{
+    for (int i = 0; i < inarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inarray.GetLength(1); j++)
+        {
+            Console.Write($"{inarray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}

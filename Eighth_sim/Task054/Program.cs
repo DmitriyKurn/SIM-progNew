073273f2ceda414f5486ce4e8a1a3a68d376ee31
Dmitriy <skverdv@gmail.com>
2023-07-06 12:11:02 +0300

    Task054

---
+++ Eighth_sim/Task054/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+Console.Write("введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите диапазон: от 1 до ");
+int range = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+GetArray(array);
+PrintArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+SortingArrayLines(array);
+PrintArray(array);
+
+void SortingArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void PrintArray(int[,] inarray)
+{
+    for (int i = 0; i < inarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inarray.GetLength(1); j++)
+        {
+            Console.Write($"{inarray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}

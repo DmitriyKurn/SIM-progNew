32b556cb47603206829499d834a96f31570bee47
Dmitriy <skverdv@gmail.com>
2023-07-10 14:23:53 +0300

    Task066

---
+++ Ninth_sim/Task066/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{SumElements(m, n)}"); 
+
+int SumElements(int start, int end)
+{
+  if (start == end) return start;
+  else return SumElements(start + 1, end) + start;
+}
+
+
+
